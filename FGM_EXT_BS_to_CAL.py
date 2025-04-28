7a826a6d42e124ef3f44cc742bc0c2dac2a6c4a5
cmcarr <c.m.carr@imperial.ac.uk>
2025-04-28 09:43:11 +0100

    Moved paths to separate .py file

--- FGM_EXT_BS_to_CAL.py
+++ FGM_EXT_BS_to_CAL.py
@@ -1,3 +1,4 @@
+#%% 
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jul 28 11:35:50 2024
@@ -22,41 +23,13 @@ Created on Sun Jul 28 11:35:50 2024
 #
 #
 #
-# craft and index variables (to change)
-
-
-craft = 'C1'
-
-#index = 400
-
-entry_date = '20020123'
-#
-#
-#
-#
-#
-#
-#
 
 
 # the rest of the program 
 
 # importing packages
 
-# add your library folder to path
-
-lib_path = 'C:/FGMEXT/Lib/'
-#lib_path = 'C:/Users/Test/Documents/FGM_Extended_Mode/Lib/'
-
-calparams_filepath = 'C:/FGM_Extended_Mode/calibration/'
-#calparams_filepath = 'C:/Users/Test/Documents/FGM_Extended_Mode/calibration'
-
-BS_filepath = 'C:/FGM_Extended_Mode/BS_raw_files/'
-
-
-# save location for output data
-filebase_cal = 'C:/FGM_Extended_Mode/' + craft + '_EXT_Calibrated/'
-
+from fgmfilepaths import craft,entry_date,lib_path,calparams_filepath,BS_filepath,filebase_cal
 
 import numpy as np
 
@@ -70,10 +43,7 @@ import os, fnmatch
 
 from datetime import datetime, timedelta
 
-
-
-
-
+#%%
 # defining functions
 
 #s16 changes unsigned 16 bit hex numbers to signed (positive and negative)
@@ -211,7 +181,7 @@ def packet_decoding_odd(ext_bytes):
     return df_p
 
 
-plt.rcParams['lines.linewidth'] = 0
+plt.rcParams['lines.linewidth'] = 1
 #plt.rcParams['lines.marker'] = '.'
 #plt.rcParams['lines.markersize'] = 1
 
@@ -301,8 +271,6 @@ validphid=(0x1F,0x47,0x6F,0x97,0x26,0x4E,0x76,0x9E,0x2D,0x55,0x7D,0xA5)
 sciphid=(0x1F,0x47,0x6F,0x97,0x26,0x4E,0x76,0x9E)
 fgmhkphid=(0x2D,0x55,0x7D,0xA5)
 
-
-
 starts_stops_spins_df = pd.read_csv(lib_path + craft + '_SATT_start_stop_spins',names = ['Starts', 'Stops', 'Spins'])
 
 ext_entries_df = pd.read_csv(lib_path + craft + '_Ext_Entries', header = None)
@@ -377,8 +345,6 @@ if duration <= timedelta(seconds = 0):
         
     raise Exception("Negatve/Zero Duration")
         
-
-
 late_half = False
 early_half = False
 
@@ -428,13 +394,14 @@ year = ext_entry.strftime('%y')
 
 month = ext_entry.strftime('%m')
 
-
+#%%
 
 formatted_entry = ext_entry.strftime('%Y%m%d')
 
 formatted_exit = ext_exit.strftime('%Y%m%d')
 
 cal_filename = find_cal_file(formatted_entry, formatted_exit,  calparams_filepath)
+print('Calibration File \n',cal_filename)
 
 cal_params = pd.read_csv(cal_filename, header = None, sep = ',|:', names = ['param', 'x', 'y', 'z'], on_bad_lines = 'skip', engine = 'python') 
 
@@ -473,7 +440,6 @@ calparams = {'x_offsets':  x_offsets,\
              'yz_gains':   yz_gains}
 
 del x_offsets, x_gains, y_gains, z_gains
-
     
 class packet():
 
@@ -523,8 +489,8 @@ class packet():
     def __str__(self):
         return("{:7s}".format("#"+str(self.pktcnt))+" | "+" ".join('{:02X}'.format(n) for n in self.cdds)+" | "+" ".join('{:02X}'.format(n) for n in self.payload[0:30]))
 
-
 BS_filename = find_BS_file(dumpdate[2:], craft, BS_filepath)
+print('Burst Science file \n',BS_filename)
 
 file = open(BS_filename,"rb")
 
@@ -534,7 +500,7 @@ del BS_filename, BS_filepath, calparams_filepath, cal_filename
 # D Burst Science packets have size 2232
 # Normal Science and Data Dump (aka Extended Mode ?) both have size 3596
 
-
+#%%
 data=bytearray(file.read())
 file.close()
 datalen=len(data)    
@@ -576,8 +542,6 @@ valid_scets = [i.scet for i in valid_ext_packets]
 
 del data, datalen, packets
 
-
-
 plt.plot(ext_nums, ext_resets, label = 'all', markersize = 1)
 plt.plot(valid_nums, valid_ext_resets, label = 'valid', marker = 'x', markersize = 1)
 plt.title('Packet Resets')
@@ -590,6 +554,8 @@ plt.plot(valid_scets, valid_nums,  label = 'valid')
 plt.title('Packet SCETs')
 plt.legend()
 plt.show()
+
+#%%
 
 # Use SCETS to look at multiple dataset case
 
@@ -639,6 +605,7 @@ plt.title('AF and BEF indices (missing ends, missing starts)')
 plt.legend()
 plt.show()
 
+#%%
 
 for i in af_indices:
         
@@ -689,7 +656,11 @@ t = make_t(ext_entry, t_spin, ext_exit, x)
 
 name = craft + '_' + datadate
 
+#%%
+
 quickplot(name + ' Raw Timestamped','time [UTC]','count [#]')
+
+#%%
 
 # nominal scaling
 # nominal change from engineering units to nanotesla
@@ -700,6 +671,8 @@ y = y * (2*64/2**15) * 4**(r-2) * (np.pi/4)
 z = z * (2*64/2**15) * 4**(r-2) * (np.pi/4)
 
 quickplot(name + ' Nominal Scaling','time [UTC]','count [#]')
+
+#%%
 
 # apply approximate cal using orbit cal see notes 30-Jan-24
 
@@ -713,6 +686,7 @@ for i in range(0,len(t)):
     
 quickplot(name+'_calibrated','time [UTC]','[nT]')
 
+#%%
 
 zSCS = np.copy(x)
 xSCS = np.copy(-y)
@@ -723,7 +697,7 @@ z = zSCS
 
 quickplot(name+'_nominal_scs','time [UTC]','[nT]')
 
-
+#%%
 degrees = 146.5
 theta = 2*np.pi*degrees/360
 xx,yy = np.copy(x),np.copy(y)
@@ -731,7 +705,7 @@ x = xx*np.cos(theta) - yy*np.sin(theta)
 y = xx*np.sin(theta) + yy*np.cos(theta)
 
 quickplot(name +'_rotated_scs','time [UTC]','[nT]')
-
+#%%
 
 
 # Eliminating anomalous data points (more than 3 standard deviations beyond the mean)
@@ -752,6 +726,7 @@ r = np.delete(r, ordered_outliers)
 
 quickplot(name +'_cleaned','time [UTC]','[nT]')
 
+#%%
 
 start_time = t[0].strftime('%Y%m%d_%H%M%S')
 
@@ -811,3 +786,5 @@ f.close()
 
 
 
+
+# %%
